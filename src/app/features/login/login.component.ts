dda0e2aa7db4e283a6d8c10b7e58c1e5231867aa
Igor <igor06brw@gmail.com>
2023-06-01 19:11:01 +0200

    clear all unnecessary code from login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,34 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Login } from './login';
-import { NgForm } from '@angular/forms';
-import { DataService } from './data.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  userSettings: Login = {
-    firstName: '',
-    password: '',
-    login: ''
-  }
-
-  constructor(private dataService: DataService) {
-
-  }
-
-
-
-  ngOnInit(): void {
-    
-  }
-
-  onSubmit(form: NgForm) {
-    console.log('in onsubmit', form.valid)
-    this.dataService.postUserSettingsForm(this.userSettings).subscribe(result => console.log('success', result), error => console.log('error', error));
-  }
+export class LoginComponent {
 
 }
--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -13,7 +13,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     LoginRoutingModule,
-    FormsModule,
     
   ]
 })
