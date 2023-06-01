8675dac15d08e196ac4bbe18795710b2b7be047c
Igor <igor06brw@gmail.com>
2023-06-01 20:31:08 +0200

    create child-parent relation between component

--- src/app/shared/validator/validator.component.ts
+++ src/app/shared/validator/validator.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-validator',
   templateUrl: './validator.component.html',
   styleUrls: ['./validator.component.scss']
 })
-export class ValidatorComponent {
+export class ValidatorComponent implements OnInit {
+  @Input() validator: any;
+
+  ngOnInit () {
+    console.log(this.validator)
+  }
+
 
 }
