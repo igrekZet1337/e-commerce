984d97ada76ee2f96c8e203d37857f31532c7c13
Igor <igor06brw@gmail.com>
2023-06-07 11:02:08 +0200

    [NAV] ver 1.0.0

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IconDefinition, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  faArrowRightToBracket: IconDefinition = faArrowRightToBracket
+  
 }
