35a21a7fa5ca1112853d94f603fb786e7ad6f05d
Igor <igor06brw@gmail.com>
2023-06-07 12:14:43 +0200

    create jumbotron, init ng-gallery

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Gallery, ImageItem } from 'ng-gallery';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private gallery: Gallery) {
+  }
+  galleryRef = this.gallery.ref('gallery')
+  
+  
+  
+  
+  ngOnInit() {
+    this.galleryRef.load([
+      new ImageItem({ src: '../../assets/images/audi.jpg' }),
+      new ImageItem({ src: '../../assets/images/volvo.jpg' }),
+    ])
+  }
+
+  
 
 }
