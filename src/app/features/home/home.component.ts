471a3d7d04ded6cfec7cc184f4bc11fe11b72c85
Igor <igor06brw@gmail.com>
2023-06-08 00:19:04 +0200

    almost finish home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnInit, ViewChild } from '@angular/core';
 import { Gallery, ImageItem } from 'ng-gallery';
 
 
@@ -8,21 +8,35 @@ import { Gallery, ImageItem } from 'ng-gallery';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   constructor(private gallery: Gallery) {
   }
   galleryRef = this.gallery.ref('gallery')
   
-  
-  
-  
-  ngOnInit() {
+  currIndex: any
+  ngOnInit(): void {
     this.galleryRef.load([
-      new ImageItem({ src: '../../assets/images/audi.jpg' }),
-      new ImageItem({ src: '../../assets/images/volvo.jpg' }),
+      new ImageItem({ src: '../../assets/images/audi.jpg', id: 'audi' }),
+      new ImageItem({ src: '../../assets/images/volvo.jpg', id: 'volvo' }),
     ])
+    this.currIndex = -1;
   }
 
-  
+  ngAfterViewInit(): void {
+    this.currIndex = 0;
+  }
 
+  indexObserver(number: any) {
+    this.currIndex = number?.currIndex;
+    console.log(number)
+  }
+}
+
+
+export interface GalleryState {
+  behavior?: ScrollBehavior;
+  currIndex?: number;
+  hasNext?: boolean;
+  hasPrev?: boolean;
+  isPlaying?: boolean;
 }
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { GalleryModule } from 'ng-gallery';
@@ -13,7 +12,7 @@ import { GalleryModule } from 'ng-gallery';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    GalleryModule
+    GalleryModule,
   ]
 })
 export class HomeModule { }
