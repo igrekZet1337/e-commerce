3a3b0e934992e0792ac44a27cb1f286e52050240
Igor <igor06brw@gmail.com>
2023-06-08 10:52:44 +0200

    [HOME] Almost finish

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,42 +1,53 @@
-import { AfterViewInit, Component, EventEmitter, OnInit, ViewChild } from '@angular/core';
-import { Gallery, ImageItem } from 'ng-gallery';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { Gallery, GalleryComponent, GalleryRef, GalleryState, ImageItem } from 'ng-gallery';
 
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  constructor(private gallery: Gallery) {
-  }
-  galleryRef = this.gallery.ref('gallery')
   
-  currIndex: any
+  
+  galleryRef: GalleryRef | undefined = this.gallery.ref('gallery')
+  currIndex: number | undefined
+
+
+  
+  @ViewChild(GalleryComponent, { read: ElementRef }) galleryHtmlTree: ElementRef<HTMLElement> | undefined
+  
+  constructor(private gallery: Gallery, private cdr: ChangeDetectorRef) { }
+  
+
   ngOnInit(): void {
-    this.galleryRef.load([
+    this.galleryRef?.load([
       new ImageItem({ src: '../../assets/images/audi.jpg', id: 'audi' }),
       new ImageItem({ src: '../../assets/images/volvo.jpg', id: 'volvo' }),
     ])
-    this.currIndex = -1;
   }
 
   ngAfterViewInit(): void {
     this.currIndex = 0;
+    this.getTemplateClass(this.currIndex)
+    console.log(this.currIndex)
+    this.cdr.detectChanges();
   }
 
-  indexObserver(number: any) {
-    this.currIndex = number?.currIndex;
-    console.log(number)
+  public indexObserver($EVENT: GalleryState | undefined) {
+    this.currIndex = $EVENT?.currIndex || 0;
+    this.getTemplateClass(this.currIndex)
   }
-}
 
+  public getTemplateClass(index: number): void {
+    const galleryTree: HTMLElement | undefined = this.galleryHtmlTree?.nativeElement;
+    const imgGalleryTree: (NodeListOf<HTMLImageElement>) | undefined = galleryTree?.querySelectorAll('img')
+    imgGalleryTree?.[index].classList.add('fetched')
+    this.cdr.detectChanges();
+    console.log(imgGalleryTree?.[index])
+    console.log()
 
-export interface GalleryState {
-  behavior?: ScrollBehavior;
-  currIndex?: number;
-  hasNext?: boolean;
-  hasPrev?: boolean;
-  isPlaying?: boolean;
+  }
 }
