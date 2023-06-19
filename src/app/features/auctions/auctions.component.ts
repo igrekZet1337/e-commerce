16bdc557071e5f18fc52ec92445ff9eabfd7195a
Igor <igor06brw@gmail.com>
2023-06-19 11:41:09 +0200

    generate component, module, route files for auctions

---
+++ src/app/features/auctions/auctions.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'mt-auctions',
+  templateUrl: './auctions.component.html',
+  styleUrls: ['./auctions.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class AuctionsComponent {
+
+}
