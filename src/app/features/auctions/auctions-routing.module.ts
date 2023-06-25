ae06f5fecb9d5ffdddc3fc11bf749934d008e8d3
Igor <igor06brw@gmail.com>
2023-06-25 10:43:54 +0200

    add full  match path for auctions

--- src/app/features/auctions/auctions-routing.module.ts
+++ src/app/features/auctions/auctions-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuctionsComponent} from "./auctions.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: AuctionsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
